cb548949d1346916b7a8beaabc0528f18e329d94
Gary9864 <00021925@uca.edu.sv>
2025-06-10 10:41:12 -0600

    ejer100

---
+++ tab.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n1,n2;
+    cout << "ingresa el primer numero" << endl;
+    cin >> n1;
+    cout << "ingresa el segundo numero" << endl;
+    cin >> n2;
+
+    if (n1<n2 )
+    {
+        cout << "El numero: "<< n2 << " esta mas cerca de 100" << endl;
+    }else{
+        cout << "El numero: "<< n1 << " esta mas cerca de 100"<< endl;
+    }
+    
+    
+
+
+
+
+
+    return 0;
+}
